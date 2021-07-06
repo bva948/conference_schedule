885d1234014a2da231cca3c5d363ea69f6c04b28
bva <hochochoc999@gmail.com>
2021-07-06 11:10:50 +0700

    finish registration models

--- conference_registration/admin.py
+++ conference_registration/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Conference, ConferenceRoom, Tag
 
-# Register your models here.
+admin.site.register(Conference)
+admin.site.register(ConferenceRoom)
+admin.site.register(Tag)
--- conference_registration/models.py
+++ conference_registration/models.py
@@ -1,23 +1,17 @@
 from django.db import models
-
-class Person(models.Model):
-    id=models.AutoField(primary_key=True)
-    name=models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
+from django.contrib.auth.models import User
 
 class ConferenceRoom(models.Model):
     id=models.AutoField(primary_key=True)
     name=models.CharField(max_length=50)
-    
+
 
     def __str__(self):
         return self.name
 
 class Tag(models.Model):
     id=models.AutoField(primary_key=True)
-    name=models.CharField(max_length=20)
+    name=models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
@@ -25,13 +19,13 @@ class Tag(models.Model):
 
 class Conference(models.Model):
     id=models.AutoField(primary_key=True)
-    name=models.CharField(max_length=100)
-    date=models.DateField()
-    start_time=models.TimeField()
-    end_time=models.TimeField()
+    name=models.CharField(max_length=150)
+    username=models.CharField(max_length=150)
+    start_time=models.DateTimeField()
+    end_time=models.DateTimeField()
     room=models.ForeignKey(ConferenceRoom, on_delete=models.CASCADE)
-    participants=models.ManyToManyField(Person)
-    tags=models.ForeignKey(Tag)
+    participants=models.ManyToManyField(User)
+    tags=models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.name
